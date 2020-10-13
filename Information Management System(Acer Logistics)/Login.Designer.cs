32501653ca7bfee45cf5e967906793d27f4b82b4
HlulaniR <71436045+HlulaniR@users.noreply.github.com>
2020-10-13 17:16:41 +0200

    Updates

--- Information Management System(Acer Logistics)/Login.Designer.cs
+++ Information Management System(Acer Logistics)/Login.Designer.cs
@@ -28,153 +28,149 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.button1 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.txtID = new System.Windows.Forms.TextBox();
-			this.txtPassword = new System.Windows.Forms.TextBox();
-			this.label6 = new System.Windows.Forms.Label();
-			this.lblFP = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.showCheckBox = new System.Windows.Forms.CheckBox();
-			this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(113, 207);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "Login";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Cooper Std Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.Color.DarkCyan;
-			this.label1.Location = new System.Drawing.Point(131, 44);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(57, 19);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Login";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(39, 108);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(110, 13);
-			this.label3.TabIndex = 3;
-			this.label3.Text = "Enter your user name:";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(89, 143);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(56, 13);
-			this.label4.TabIndex = 4;
-			this.label4.Text = "Password:";
-			// 
-			// txtID
-			// 
-			this.txtID.Location = new System.Drawing.Point(152, 101);
-			this.txtID.Name = "txtID";
-			this.txtID.Size = new System.Drawing.Size(100, 20);
-			this.txtID.TabIndex = 6;
-			// 
-			// txtPassword
-			// 
-			this.txtPassword.Location = new System.Drawing.Point(152, 135);
-			this.txtPassword.Name = "txtPassword";
-			this.txtPassword.PasswordChar = '*';
-			this.txtPassword.Size = new System.Drawing.Size(100, 20);
-			this.txtPassword.TabIndex = 7;
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
-			this.label6.Location = new System.Drawing.Point(87, 178);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(62, 15);
-			this.label6.TabIndex = 9;
-			this.label6.Text = "New user?";
-			this.label6.Click += new System.EventHandler(this.label6_Click);
-			// 
-			// lblFP
-			// 
-			this.lblFP.AutoSize = true;
-			this.lblFP.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblFP.ForeColor = System.Drawing.SystemColors.Highlight;
-			this.lblFP.Location = new System.Drawing.Point(181, 178);
-			this.lblFP.Name = "lblFP";
-			this.lblFP.Size = new System.Drawing.Size(97, 15);
-			this.lblFP.TabIndex = 10;
-			this.lblFP.Text = "Forgot password?";
-			this.lblFP.Visible = false;
-			this.lblFP.Click += new System.EventHandler(this.label7_Click);
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-			this.label5.Location = new System.Drawing.Point(91, 248);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(125, 13);
-			this.label5.TabIndex = 11;
-			this.label5.Text = "Please enter your details!";
-			// 
-			// showCheckBox
-			// 
-			this.showCheckBox.AutoSize = true;
-			this.showCheckBox.Location = new System.Drawing.Point(272, 137);
-			this.showCheckBox.Name = "showCheckBox";
-			this.showCheckBox.Size = new System.Drawing.Size(51, 17);
-			this.showCheckBox.TabIndex = 12;
-			this.showCheckBox.Text = "show";
-			this.showCheckBox.UseVisualStyleBackColor = true;
-			this.showCheckBox.CheckedChanged += new System.EventHandler(this.showCheckBox_CheckedChanged);
-			// 
-			// errorProvider1
-			// 
-			this.errorProvider1.ContainerControl = this;
-			// 
-			// Login
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(335, 332);
-			this.Controls.Add(this.showCheckBox);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.lblFP);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.txtPassword);
-			this.Controls.Add(this.txtID);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button1);
-			this.Name = "Login";
-			this.Text = "Login";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblFP = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.showCheckBox = new System.Windows.Forms.CheckBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 255);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter your user name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(203, 124);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(132, 22);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(203, 166);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(132, 22);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(116, 219);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "New user?";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblFP
+            // 
+            this.lblFP.AutoSize = true;
+            this.lblFP.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFP.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblFP.Location = new System.Drawing.Point(241, 219);
+            this.lblFP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFP.Name = "lblFP";
+            this.lblFP.Size = new System.Drawing.Size(120, 17);
+            this.lblFP.TabIndex = 10;
+            this.lblFP.Text = "Forgot password?";
+            this.lblFP.Visible = false;
+            this.lblFP.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(121, 305);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Please enter your details!";
+            // 
+            // showCheckBox
+            // 
+            this.showCheckBox.AutoSize = true;
+            this.showCheckBox.Location = new System.Drawing.Point(363, 169);
+            this.showCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.showCheckBox.Name = "showCheckBox";
+            this.showCheckBox.Size = new System.Drawing.Size(62, 21);
+            this.showCheckBox.TabIndex = 12;
+            this.showCheckBox.Text = "show";
+            this.showCheckBox.UseVisualStyleBackColor = true;
+            this.showCheckBox.CheckedChanged += new System.EventHandler(this.showCheckBox_CheckedChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 409);
+            this.Controls.Add(this.showCheckBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblFP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.TextBox txtID;
